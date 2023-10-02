04b0c35a7bb6d3a012e2ec13b39dae604db7c649
Agada Francis Louis N <agadafrancis91@gmail.com>
2023-10-02 23:54:44 +0100

    Update AccountProfile.tsx

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
+import * as z from "zod";
 
 interface Props {
     user: {
@@ -38,10 +39,32 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             bio: ''
         }
     })
-    return (
-        <Form>
 
-        </Form>
+
+      // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+    return (
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
     )
 }
 
