be2e2aad99515bb98b7a1750d1013075a268585e
Agada Francis Louis N <agadafrancis91@gmail.com>
2023-10-02 23:46:17 +0100

    shad

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,8 +1,19 @@
 "use client"
 
-import { Form } from '@/components/ui/form';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
 
 interface Props {
     user: {
@@ -19,7 +30,13 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
     
     const form = useForm({
-        resolver: zodResolver()
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: ''
+        }
     })
     return (
         <Form>
