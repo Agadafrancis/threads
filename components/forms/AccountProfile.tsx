cb459370bb2be1ed77ae139141ade62d88e02e63
Agada Francis Louis N <agadafrancis91@gmail.com>
2023-10-03 01:07:01 +0100

    Update AccountProfile.tsx

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -102,11 +102,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+              <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
                     Name
                 </FormLabel>
-                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <FormControl>
                   <Input
                     type="text" 
                     className="account-form_input no-focus"
@@ -121,11 +121,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="username"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+                <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
                     Username
                 </FormLabel>
-                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <FormControl>
                   <Input
                     type="text" 
                     className="account-form_input no-focus"
@@ -140,11 +140,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="bio"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+                <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
                     Bio
                 </FormLabel>
-                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <FormControl>
                   <Textarea
                     rows={10} 
                     className="account-form_input no-focus"
