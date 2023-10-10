487e313fc4a5633eb48cd97cf91ed091f278a6c5
Agada Francis Louis N <agadafrancis91@gmail.com>
2023-10-10 23:57:02 +0100

    Update page.tsx

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,3 +1,6 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 async function Page() {
     return(
         <h1 className="head-text">Create thread</h1>
